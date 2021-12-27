7efdd7fddeef91973e8e5888d689235192b129cf
tzuyi <a0988426059@gmail.com>
2021-12-27 18:13:54 +0800

    promise then callback hell

--- promise.js
+++ promise.js
@@ -40,40 +40,70 @@ class PrivatePromise {
     typeof onFulfilled !== "function" && (onFulfilled = (value) => value);
     typeof onRejected !== "function" && (onRejected = (reason) => { throw reason });
 
-    switch (this.PromiseState) {
-      case "pending":
-        this.onFulfilledCallback.push(onFulfilled);
-        this.onRejectedCallback.push(onRejected);
-        break;
-      case "fulfilled":
-        setTimeout(() => {
-          onFulfilled(this.PromiseResult);
-        });
-        break;
-      case "rejected":
-        setTimeout(() => {
-          onRejected(this.PromiseResult);
-        });
-        break;
-    }
+    const callbackPromise = new PrivatePromise((resolve, reject) => {
+      switch (this.PromiseState) {
+        case "pending":
+          this.onFulfilledCallback.push(() => {
+            processPromise(callbackPromise, resolve, reject, onFulfilled(this.PromiseResult));
+          });
+          this.onRejectedCallback.push(() => {
+            processPromise(callbackPromise, resolve, reject, onRejected(this.PromiseResult));
+          });
+          break;
+        case "fulfilled":
+          processPromise(callbackPromise, resolve, reject, onFulfilled(this.PromiseResult));
+          break;
+        case "rejected":
+          processPromise(callbackPromise, resolve, reject, onRejected(this.PromiseResult));
+          break;
+      }
+    });
   }
 };
 
-console.log("start");
+function processPromise() {
+  const [promise, resolve, reject, callback] = arguments;
 
-const promise = new PrivatePromise((resolve, reject) => {
-  console.log("promise")
   setTimeout(() => {
-    resolve("123");
-    console.log("setTimeout");
-  })
+    try {
+      if (promise === callback) return reject(new TypeError("Chaining cycle detected for promise")); // 避免引用同個 promise 報錯
+      if (callback instanceof PrivatePromise) {
+        switch (callback.PromiseState) {
+          case "pending":
+            callback.then(result => {
+              processPromise(promise, resolve, reject, result);
+            }, reject);
+            break;
+          case "fulfilled":
+            resolve(callback.PromiseResult);
+            break;
+          case "rejected":
+            reject(callback.PromiseResult);
+            break;
+        }
+      } else {
+        resolve(callback);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const promise = new Promise((resolve, reject) => {
+  resolve("resolve")
 });
 
-
-promise.then(result => {
-  console.log(result);
-}, reason => {
-  console.log(reason);
-});
-
-console.log("end");
+promise.then(value => {
+  console.log(value)
+  return value + "1";
+}).then(value => {
+  console.log(value)
+  return value + "2";
+}).then(value => {
+  console.log(value)
+  return value + "3";
+}).then(value => {
+  console.log(value)
+  return value + "4";
+})
