223fb9c44fbf7645f193e23513a1c912092579a2
tzuyi <a0988426059@gmail.com>
2021-12-29 10:51:12 +0800

    fix bug

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 Array.prototype.privateMap = function(callback, thisArg) {
-  if (this == null) throw TypeError("this is null or not defined");
+  if (this == null) throw new TypeError("this is null or not defined");
   if (typeof callback !== "function") throw new TypeError(`${callback} is not a function`);
 
   const array = Object(this);
