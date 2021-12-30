ec61e179c810b170118b2094c8d984952d1f4d0d
tzuyi <a0988426059@gmail.com>
2021-12-30 10:00:57 +0800

    feat reduce

---
+++ javascript/reduce.js
@@ -0,0 +1,30 @@
+Array.prototype.privateReduce = function (callback, initialValue) {
+  if (this == null) throw new TypeError("this is null or not defined");
+  if (typeof callback !== "function") throw new TypeError(`${callback} is not a function`);
+
+  const array = Object(this);
+  let accumulator = initialValue;
+  let k = 0;
+
+  if (arguments.length === 1) {
+    while (!(k in array)) {
+      if (k > array.length - 1) throw new TypeError("Reduce of empty array with no initial value");
+      k += 1;
+    }
+    accumulator = array[k];
+  }
+
+  for (let index = k + 1; index < array.length; index++) {
+    if (index in array) accumulator = callback(accumulator, array[index], index, array);
+  }
+  return accumulator;
+}
+
+setTimeout(() => {
+  const reduce = [, , 5, 6].privateReduce((accumulator, currentValue, currentIndex, array) => {
+    console.log(accumulator, currentValue, currentIndex, array);
+    return accumulator + currentValue;
+  })
+
+  console.log(reduce);
+})
