b2d641079bcb5b163cf5bc83c6ec6d378c674282
tzuyi <a0988426059@gmail.com>
2021-12-30 13:16:23 +0800

    fix reduce bug

--- javascript/reduce.js
+++ javascript/reduce.js
@@ -11,20 +11,20 @@ Array.prototype.privateReduce = function (callback, initialValue) {
       if (k > array.length - 1) throw new TypeError("Reduce of empty array with no initial value");
       k += 1;
     }
-    accumulator = array[k];
+    accumulator = array[k++];
   }
 
-  for (let index = k + 1; index < array.length; index++) {
+  for (let index = k; index < array.length; index++) {
     if (index in array) accumulator = callback(accumulator, array[index], index, array);
   }
   return accumulator;
 }
 
 setTimeout(() => {
-  const reduce = [, , 5, 6].privateReduce((accumulator, currentValue, currentIndex, array) => {
+  const reduce = [15, 5, 5, 6].privateReduce((accumulator, currentValue, currentIndex, array) => {
     console.log(accumulator, currentValue, currentIndex, array);
     return accumulator + currentValue;
-  })
+  }, 10)
 
   console.log(reduce);
 })
